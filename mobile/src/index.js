ca2c2652b2398d929b488682676273851587c3d2
Aléfe Chorna <alefechorna99@gmail.com>
2019-07-01 20:53:28 -0300

    Configurado a biblioteca barcode scanner para leitura de código de barras

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
+import BarcodeScanner from 'react-native-scan-barcode';
 
 export default class App extends Component {
+  state = {
+    torchMode: 'on',
+    cameraType: 'back',
+    content: '',
+  }
+
+  barcodeReceived = (e) => {
+    this.setState({ content: e.data });
+
+    console.log('Barcode: ' + e.data);
+    console.log('Type: ' + e.type);
+  }
+
+  async componentDidMount() {
+    const rp = await 
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+  }
+
+  maskLoaderBarCode() {
+    return (
+      <View style={{
+         height: 200, 
+         justifyContent: 'center',
+         alignItems: 'center' 
+        }}>
+        <View style={{ height: 2, width: 320, backgroundColor: 'red' }} />
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <View style={styles.viewBarCode}>
+          <BarcodeScanner
+            onBarCodeRead={this.barcodeReceived}
+            showViewFinder={false}
+            style={{ flex: 1 }}
+            children={this.maskLoaderBarCode()}
+            torchMode={this.state.torchMode}
+            cameraType={this.state.cameraType} />
+        </View>
+        <View style={styles.viewContent}>
+          <Text>{this.state.content}</Text>
+        </View>
       </View>
     );
   }
@@ -14,9 +56,14 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  viewBarCode: {
+    height: 200,
+    backgroundColor: 'red',
+  },
+  viewContent: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
