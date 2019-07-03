5f726a20d29cf23a7be31f2fb1c4c6e0520813fd
Aléfe Chorna <alefechorna99@gmail.com>
2019-07-02 23:35:21 -0300

    Criado tela de Login

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,73 +1,7 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
-import BarcodeScanner from 'react-native-scan-barcode';
+import React from 'react';
 
-export default class App extends Component {
-  state = {
-    torchMode: 'on',
-    cameraType: 'back',
-    content: '',
-  }
+import Routes from './routes';
 
-  barcodeReceived = (e) => {
-    this.setState({ content: e.data });
+const App = () => <Routes />;
 
-    console.log('Barcode: ' + e.data);
-    console.log('Type: ' + e.type);
-  }
-
-  async componentDidMount() {
-    const rp = await 
-      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
-  }
-
-  maskLoaderBarCode() {
-    return (
-      <View style={{
-         height: 200, 
-         justifyContent: 'center',
-         alignItems: 'center' 
-        }}>
-        <View style={{ height: 2, width: 320, backgroundColor: 'red' }} />
-      </View>
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.viewBarCode}>
-          <BarcodeScanner
-            onBarCodeRead={this.barcodeReceived}
-            showViewFinder={false}
-            style={{ flex: 1 }}
-            children={this.maskLoaderBarCode()}
-            torchMode={this.state.torchMode}
-            cameraType={this.state.cameraType} />
-        </View>
-        <View style={styles.viewContent}>
-          <Text>{this.state.content}</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  viewBarCode: {
-    height: 200,
-    backgroundColor: 'red',
-  },
-  viewContent: {
-    flex: 1,
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
+export default App;
